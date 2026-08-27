7f2931ee57eb36d2083bdb56e290b9cd0e3bef5e
yemanetsegay92-sketch <yemanetsegay92@gmail.com>
2026-08-27 21:28:52 +0300

    Add files via upload

--- api/analyze.js
+++ api/analyze.js
@@ -22,6 +22,37 @@ API key stays on the Vercel server.
 
 
 export default async function handler(req, res) {
+      /*
+    ========================================================
+    CORS
+    ========================================================
+    */
+
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "POST, OPTIONS"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+
+    /*
+    Handle browser preflight request
+    */
+
+    if (req.method === "OPTIONS") {
+
+        return res.status(200).end();
+
+    }
 
     /*
     ========================================================
